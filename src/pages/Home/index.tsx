c4ce54863a2fe6ac77edf84938c2aa6a91a4f822
craft52 <lee666980@gmail.com>
2026-03-03 09:41:17 +0800

    添加Ant Design mobile ui框架

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { Button, Space, Toast } from 'antd-mobile';
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  const handleClick = () => {
+    Toast.show({
+      content: '点击了按钮',
+    });
+  };
+  return (
+    <Space style={{ padding: 20 }}>
+      <Button color="primary" onClick={handleClick}>
+        主要按钮
+      </Button>
+      <Button color="success">成功按钮</Button>
+      <Button color="danger">危险按钮</Button>
+    </Space>
+  );
 };
 
 export default Home;
